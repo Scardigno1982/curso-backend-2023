47b952a2e061474f625e45d2384c3e595a736554
Sergio Scardigno <sergioscardigno82@gmail.com>
2023-06-18 11:35:40 -0300

    clase 5

---
+++ practica_clase_05/src/data.js
@@ -0,0 +1,38 @@
+function obtenerZapatillas() {
+    return [
+        
+                {
+                    "id": 1,
+                    "marca": "Nike",
+                    "talle": 42,
+                    "modelo": "Air Max"
+                },
+                {
+                    "id": 2,
+                    "marca": "Adidas",
+                    "talle": 44,
+                    "modelo": "Ultra Boost"
+                },
+                {
+                    "id": 3,
+                    "marca": "Puma",
+                    "talle": 41,
+                    "modelo": "RS-X"
+                },
+                {
+                    "id": 4,
+                    "marca": "New Balance",
+                    "talle": 43,
+                    "modelo": "574"
+                },
+                {
+                    "id": 5,
+                    "marca": "Converse",
+                    "talle": 45,
+                    "modelo": "Chuck Taylor All Star"
+                }
+            ]
+        
+}
+
+module.exports = { obtenerZapatillas };
